28178103a50c4cbb51b0517fbf72d96b9c05a713
Dr. Amr Osman <dr3mro@gmail.com>
2024-06-17 01:12:40 +0300

    Add k6 benchmark.js

---
+++ benchmark.js
@@ -0,0 +1,22 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+  stages: [
+    // Ramp-up from 1 to 50 users over 30 seconds
+    { duration: '30s', target: 50 },
+    // Stay at 50 users for 1 minute
+    { duration: '1m', target: 50 },
+    // Ramp-down from 50 to 0 users over 30 seconds
+    { duration: '30s', target: 0 }
+  ],
+};
+
+export default function () {
+  let res = http.get('http://localhost:18080/get');
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+  });
+  //sleep(1); // Adjust if necessary to control request rate
+}
+
