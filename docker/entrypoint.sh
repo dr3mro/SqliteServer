a4f6d0b26986eab959c5a9f9cef0d18ce075abd8
Dr. Amr Osman <dr3mro@gmail.com>
2024-06-21 09:22:15 +0300

    add entrypoint to docker

---
+++ docker/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+# Clone the repository if it doesn't exist
+if [ ! -d .git ]; then
+    git clone https://github.com/dr3mro/ProjectValhalla.git .
+fi
+
+# Pull the latest changes
+git pull
+
+# Build the project
+cmake -GNinja .
+ninja
+
+# Execute the server
+exec ./server
