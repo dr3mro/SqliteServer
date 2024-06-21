cc65b0d5f0fc343493d282fe67158c58c4ded52c
Dr. Amr Osman <dr3mro@gmail.com>
2024-06-21 15:16:13 +0300

    update sql schema

--- docker/init.sql
+++ docker/init.sql
@@ -1,7 +1,10 @@
 -- init.sql
 CREATE TABLE IF NOT EXISTS personal_history (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    phone VARCHAR(255),
-    json JSONB
+    name VARCHAR(255) UNIQUE,
+    phone VARCHAR(255) UNIQUE,
+    json JSONB,
+    created_on TIMESTAMP WITH TIME ZONE default now(),
+    modified_on TIMESTAMP WITH TIME ZONE
 );
+
